2ccff66eb94fcff664b34a828543152fb4658318
KRotide <Cheepster38@hotmail.fr>
2023-10-24 21:23:55 +0200

    Améliorations + animations

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,34 @@
+import React, { useEffect, useState } from "react";
+
 export default function Banner() {
+    const me = "Caroline Ruiz";
+    const [displayedMe, setDisplayedMe] = useState("");
+    const [meTyped, setMeTyped] = useState(false);
+    const [infoSlide, setInfoSlide] = useState(false);
+
+    useEffect(() => {
+        let currentIndex = 0;
+        const typingInterval = setInterval(() => {
+            if (currentIndex <= me.length) {
+                setDisplayedMe(me.slice(0, currentIndex));
+                currentIndex++;
+            } else {
+                clearInterval(typingInterval);
+                setMeTyped(true);
+                setInfoSlide(true);
+            }
+        }, 200);
+        return () => clearInterval(typingInterval);
+    }, [me]);
 
     return (
         <div className="banner row">
             <div className="banner__text">
-                <h1 className="banner__text__title">
-                    <span>Caroline Ruiz</span>
+                <h1 className={`banner__text__title ${meTyped ? "meTyped" : ""}`}>
+                    <span>{displayedMe}</span>
                 </h1>
-                <p>Intégratrice web, proche de Lyon, France</p>
-                <a href="#Contact" className="banner__text__btn banner__text__btn--pink">Me contacter</a>
+                <p className={meTyped ? "infoSlide" : ""}>Intégratrice web, proche de Lyon, France</p>
+                <a href="#Contact" className={`banner__text__btn banner__text__btn--pink ${meTyped && infoSlide ? "showButton" : ""}`}>Me contacter</a>
             </div>
         </div>
     )
